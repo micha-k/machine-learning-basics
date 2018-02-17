45c7ff82b262d00cab585e668e5c87f5eb3df7fa
Michael Kessel <Michael.Kessel@epost-dev.de>
2018-02-18 00:18:38 +0100

    Refactored into a class

--- mlm-tutorial/mlm.py
+++ mlm-tutorial/mlm.py
@@ -9,16 +9,18 @@ import pandas
 import sklearn
 
 
-def get_versions():
-    print('Python: {}'.format(sys.version))
-    print('scipy: {}'.format(scipy.__version__))
-    print('numpy: {}'.format(numpy.__version__))
-    print('matplotlib: {}'.format(matplotlib.__version__))
-    print('pandas: {}'.format(pandas.__version__))
-    print('sklearn: {}'.format(sklearn.__version__))
+class MLMTutorial:
 
-def default():
-    pass
+    def get_versions(self):
+        print('Python: {}'.format(sys.version))
+        print('scipy: {}'.format(scipy.__version__))
+        print('numpy: {}'.format(numpy.__version__))
+        print('matplotlib: {}'.format(matplotlib.__version__))
+        print('pandas: {}'.format(pandas.__version__))
+        print('sklearn: {}'.format(sklearn.__version__))
+
+    def default(self):
+        pass
 
 def main():
 
@@ -26,11 +28,13 @@ def main():
         cmd = sys.argv[1]
     except IndexError as e:
         cmd = 'default'
-    
+
+    mlm = MLMTutorial()
+
     if cmd == 'versions':
-        get_versions()
+        mlm.get_versions()
     else:
-        default()
+        mlm.default()
 
 if __name__ == '__main__':
     main()
