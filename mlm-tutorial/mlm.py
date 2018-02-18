1dddcc3cb04d29d7483becfd97674c24b9189e97
Michael Kessel <Michael.Kessel@epost-dev.de>
2018-02-18 01:04:35 +0100

    Splitting dataset; evaluations methods; prediction run

--- mlm-tutorial/mlm.py
+++ mlm-tutorial/mlm.py
@@ -69,6 +69,65 @@ class MLMTutorial:
         scatter_matrix(self.dataset)
         plt.show()
 
+    # Create test dataset; compare models; plot the results
+    def test_and_compare(self):
+
+        self.split_dataset()
+
+        # settings for the test
+        seed = 7
+        scoring = 'accuracy'
+
+        # loading models and check run them
+        models = []
+        models.append(('LR', LogisticRegression()))
+        models.append(('LDA', LinearDiscriminantAnalysis()))
+        models.append(('KNN', KNeighborsClassifier()))
+        models.append(('CART', DecisionTreeClassifier()))
+        models.append(('NB', GaussianNB()))
+        models.append(('SVM', SVC()))
+        # evaluate each model in turn
+        results = []
+        names = []
+        for name, model in models:
+            kfold = model_selection.KFold(n_splits=10, random_state=seed)
+            cv_results = model_selection.cross_val_score(model, self.X_train, self.Y_train, cv=kfold, scoring=scoring)
+            results.append(cv_results)
+            names.append(name)
+            msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+            print(msg)
+
+        # plot the results
+        fig = plt.figure()
+        fig.suptitle('Algorithm Comparison')
+        ax = fig.add_subplot(111)
+        plt.boxplot(results)
+        ax.set_xticklabels(names)
+        plt.show()
+
+    def knn_predict(self):
+
+        self.split_dataset()
+
+        # Make predictions on validation dataset
+        knn = KNeighborsClassifier()
+        knn.fit(self.X_train, self.Y_train)
+        predictions = knn.predict(self.X_validation)
+        print(accuracy_score(self.Y_validation, predictions))
+        print(confusion_matrix(self.Y_validation, predictions))
+        print(classification_report(self.Y_validation, predictions))
+
+    def split_dataset(self):
+
+        # 80% - 20% split of the dataset
+        array = self.dataset.values
+        X = array[:,0:4]
+        Y = array[:,4]
+        validation_size = 0.20
+        seed = 7
+        self.X_train, self.X_validation, self.Y_train, self.Y_validation = \
+            model_selection.train_test_split(X, Y, test_size=validation_size,
+                                             random_state=seed)
 
     def get_versions(self):
         print('Python: {}'.format(sys.version))
@@ -100,6 +159,10 @@ def main():
         mlm.plot_histo()
     elif cmd == 'plot_scatter':
         mlm.plot_scatter()
+    elif cmd == 'test_compare':
+        mlm.test_and_compare()
+    elif cmd == 'knn_predict':
+        mlm.knn_predict()
     else:
         mlm.default()
 
